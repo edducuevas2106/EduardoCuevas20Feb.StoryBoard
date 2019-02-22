489273246d266e0ec53eca416c75c26d0911ac4b
edducuevas2106 <edducuevas2106@gmail.com>
2019-02-22 16:56:23 -0600

    Initial Commit

---
+++ EduardoCuevas20Feb.StoryBoardTests/EduardoCuevas20Feb_StoryBoardTests.swift
@@ -0,0 +1,34 @@
+//
+//  EduardoCuevas20Feb_StoryBoardTests.swift
+//  EduardoCuevas20Feb.StoryBoardTests
+//
+//  Created by MacBook Pro on 22/02/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import XCTest
+@testable import EduardoCuevas20Feb_StoryBoard
+
+class EduardoCuevas20Feb_StoryBoardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
